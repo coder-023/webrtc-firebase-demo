3f841df7d6c3c4957a00844cdc8294e5c25d0e67
coder-023 <mukul.borole23@gmail.com>
2022-02-10 00:55:37 +0530

    Shifted the firebase initialization code to main.js

--- webcamaccess/static/js/main.js
+++ webcamaccess/static/js/main.js
@@ -9,11 +9,23 @@ const firebaseConfig = {
   projectId: "webrtc1-b74b3",
   storageBucket: "webrtc1-b74b3.appspot.com",
   messagingSenderId: "299736381939",
-  appId: "1:299736381939:web:346815f795031392a4fa86"
+  appId: "1:299736381939:web:d03cc888b5deb6b5a4fa86"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDVr9y6YoHToa8xPu-XRTtdVcZ2mNDUXus",
+//   authDomain: "webrtc1-b74b3.firebaseapp.com",
+//   projectId: "webrtc1-b74b3",
+//   storageBucket: "webrtc1-b74b3.appspot.com",
+//   messagingSenderId: "299736381939",
+//   appId: "1:299736381939:web:346815f795031392a4fa86"
+// };
 // const app = initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 console.log(firebase);
 const firestore = firebase.firestore();
 
@@ -72,9 +84,12 @@ webcamButton.onclick = async () => {
 callButton.onclick = async () => {
   // Reference Firestore collections for signaling
   const callDoc = firestore.collection('calls').doc();
+  const callDoc1 = firestore.collection('calls1').doc();
+
   const offerCandidates = callDoc.collection('offerCandidates');
+  const offerCandidates1 = callDoc1.collection('offerCandidates');
   const answerCandidates = callDoc.collection('answerCandidates');
-console.log(offerCandidates)
+console.log(offerCandidates1)
   callInput.value = callDoc.id; //This id is displayed for us on the text BaseAudioContext, which we share to remote user
 
   // Get candidates for caller, save to db
